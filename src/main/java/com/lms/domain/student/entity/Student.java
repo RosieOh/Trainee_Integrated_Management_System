34a8c655926aa085c9b058e9c1b66c1ef4d965ca
sendjin5 <sendjin5@gmail.com>
2024-04-19 12:55:33 +0900

    [FEAT] Member 추가사항 Student 추가 #8

--- src/main/java/com/lms/domain/student/entity/Student.java
+++ src/main/java/com/lms/domain/student/entity/Student.java
@@ -1,94 +1,77 @@
-package com.lms.domain.student.entity;
+package com.lms.domain.counsel.entity;
 
-import com.lms.global.cosntant.BaseEntity;
+import com.lms.domain.student.entity.Student;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
-public class Student extends BaseEntity {
+@NoArgsConstructor
+public class Counsel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long no;
 
-    @Column(unique = true, nullable = false)
-    private String email;           // 유저가 사용하는 이메일
+    @Column(nullable = true)
+    private String address;
 
     @Column(nullable = true)
-    private String smoke;            //흡연
+    private String cert;
 
     @Column(nullable = true)
-    private String disease;            //질병
+    private String exp;
 
     @Column(nullable = true)
-    private String accident;            //사고
+    private String stack;
 
     @Column(nullable = true)
-    private String etc;            //기타
+    private String OA;
 
     @Column(nullable = true)
-    private String reason;            //사유
+    private String content;
 
     @Column(nullable = true)
-    private String score;            //벌점
+    private String satis;
 
     @Column(nullable = true)
-    private String date;            //날짜
+    private String edu_data;
 
     @Column(nullable = true)
-    private String education;            //교육수료
+    private String instructor;
 
     @Column(nullable = true)
-    private String certi;            //자격증
+    private String task;
 
     @Column(nullable = true)
-    private String workshop;            //워크샵
+    private String env;
 
     @Column(nullable = true)
-    private String cv;            //자기소개서
+    private String goal;
 
     @Column(nullable = true)
-    private String resume;            //이력서
+    private String doc;
 
     @Column(nullable = true)
-    private String univ;            //대학교
+    private String personal;
 
     @Column(nullable = true)
-    private String major;           //학과
+    private String opinion;
 
     @Column(nullable = true)
-    private String job_program;     //국취참여여부
+    private String etc;
 
     @Column(nullable = true)
-    private String fund;            //지원센터
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    @JoinColumn(name = "couno", referencedColumnName = "no")
+    private Student student;   // 학생
 
     @Column(nullable = true)
-    private String cert;            //수료증번호
-
-    @Column(nullable = true)
-    private String picture;         //프로필 사진
-
-    @Column(nullable = true)
-    private String complete;        //수료여부
-
-    @Column(nullable = true)
-    private String depart;        //부서
-
-    @Column(nullable = true)
-    private String hope;        //희망부서
-
-    @Column(nullable = true)
-    private String early;        //조기인턴유무
-
-    @Column(nullable = true)
-    private String other;        //타기업취업
-
-    @Column(nullable = true)
-    private String portfolio;        //포트폴리오
-
+    private LocalDateTime date;
 }
