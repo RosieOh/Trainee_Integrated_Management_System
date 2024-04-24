81e445a58728c47e4189a9d1ee437806ae67cc4c
RosieOh <dhxogns920@gmail.com>
2024-04-24 13:46:32 +0900

    ✨FEAT : Amazone S3 파일 처리 시스템 로직 작성 - 키값 오류로 인한 주석 처리 #29

---
+++ src/main/java/com/lms/global/s3/service/MediaUpload.java
@@ -0,0 +1,16 @@
+//package com.lms.global.s3.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.Value;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class MediaUpload implements MediaUploadInterface {
+//
+//    @Value("${cloud.aws.s3.bucket}")
+//    private String bucket;
+//
+//
+//}
+//
