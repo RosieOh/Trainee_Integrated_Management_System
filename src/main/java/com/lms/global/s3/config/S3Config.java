979efe5f3aa275273367c35435b14513b9879edd
RosieOh <dhxogns920@gmail.com>
2024-04-25 16:46:36 +0900

    ✨CHORE : Amazone S3 버전 변경으로 인한 Config 수정#29

--- src/main/java/com/lms/global/s3/config/S3Config.java
+++ src/main/java/com/lms/global/s3/config/S3Config.java
@@ -4,11 +4,15 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@ConfigurationProperties(prefix = "lms")
 @Configuration
 public class S3Config {
     @Value("${cloud.aws.credentials.access-key}")
@@ -19,9 +23,10 @@ public class S3Config {
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        return AmazonS3ClientBuilder.standard()
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return (AmazonS3Client) AmazonS3ClientBuilder
                 .standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
