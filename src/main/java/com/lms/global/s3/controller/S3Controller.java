9102d8413493cc1a8d4ee20d2392f7a0dd98bcbe
RosieOh <dhxogns920@gmail.com>
2024-05-07 14:16:52 +0900

    ✨CHORE : S3 수정으로 인한 Main Branch Merge #65

--- src/main/java/com/lms/global/s3/controller/S3Controller.java
+++ src/main/java/com/lms/global/s3/controller/S3Controller.java
@@ -1,46 +1,57 @@
-// package com.lms.global.s3.controller;
+//package com.lms.global.s3.controller;
 //
-// import com.lms.global.s3.dto.FileDetail;
-// import com.lms.global.s3.service.FileDownloadService;
-// import com.lms.global.s3.service.FileUploadService;
-// import lombok.RequiredArgsConstructor;
-// import lombok.extern.log4j.Log4j2;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-// import org.springframework.web.multipart.MultipartFile;
+//import com.lms.global.s3.dto.FileDetail;
+//import com.lms.global.s3.service.FileDownloadService;
+//import com.lms.global.s3.service.FileUploadService;
+//import com.lms.global.s3.service.S3Service;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
 //
-// import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+//import java.util.List;
 //
-// @Log4j2
-// @RestController
-// @RequestMapping(value = "/upload", produces = APPLICATION_JSON_VALUE)
-// @RequiredArgsConstructor
-// public class S3Controller {
+//import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 //
-//     private final FileUploadService fileUploadService;
-//     private final FileDownloadService fileDownloadService;
+//@Log4j2
+//@RestController
+//@RequestMapping(value = "/upload", produces = APPLICATION_JSON_VALUE)
+//@RequiredArgsConstructor
+//public class S3Controller {
 //
-//     @PostMapping
-//     public ResponseEntity<FileDetail> post(@RequestPart(value = "file", required = false) MultipartFile multipartFile) {
-//         if (multipartFile == null || multipartFile.isEmpty()) {
-//             // 파일이 첨부되지 않았을 때
-//             log.info("파일이 첨부되지 않았습니다.");
-//             return ResponseEntity.badRequest().body(null);
-//         } else {
-//             // 파일이 첨부되었을 때
-//             log.info("파일이 성공적으로 첨부되었습니다.");
-//             return ResponseEntity.ok(fileUploadService.save(multipartFile));
-//         }
-//     }
+//    private final FileUploadService fileUploadService;
+//    private final FileDownloadService fileDownloadService;
+//    private final S3Service s3Service;
 //
-//     @GetMapping("/download")
-//     public ResponseEntity<byte[]> download(@RequestParam String fileName) {
-//         try {
-//             return fileDownloadService.getObject(fileName);
-//         } catch (Exception e) {
-//             log.error("Error while downloading file: {}", e.getMessage());
-//             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-//         }
-//     }
-// }
+//    @PostMapping
+//    public ResponseEntity<FileDetail> post(@RequestPart(value = "file", required = false) MultipartFile multipartFile) {
+//        if (multipartFile == null || multipartFile.isEmpty()) {
+//            // 파일이 첨부되지 않았을 때
+//            log.info("파일이 첨부되지 않았습니다.");
+//            return ResponseEntity.badRequest().body(null);
+//        } else {
+//            // 파일이 첨부되었을 때
+//            log.info("파일이 성공적으로 첨부되었습니다.");
+//            return ResponseEntity.ok(fileUploadService.save(multipartFile));
+//        }
+//    }
+//
+//    @GetMapping("/download")
+//    public ResponseEntity<byte[]> download(@RequestParam String fileName) {
+//        try {
+//            return fileDownloadService.getObject(fileName);
+//        } catch (Exception e) {
+//            log.error("Error while downloading file: {}", e.getMessage());
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+//        }
+//    }
+//
+//    // profile 이미지 전달
+//    @GetMapping("/member/profile")
+//    public List<String> getProfileImages() {
+//        List<String> profileImageList = s3Service.getFileList("profile");
+//        return profileImageList;
+//    }
+//}
