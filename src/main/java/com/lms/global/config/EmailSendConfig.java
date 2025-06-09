3b5a5357191c9b062ff227e87dd87c9352ed01a7
RosieOh <dhxogns920@gmail.com>
2025-06-09 13:36:52 +0900

    CHORE : 이메일 인증사항 변경

--- src/main/java/com/lms/global/config/EmailSendConfig.java
+++ src/main/java/com/lms/global/config/EmailSendConfig.java
@@ -3,48 +3,46 @@ package com.lms.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
 @Configuration
-@PropertySource("classpath:email.properties")
-
-// 1일 500회 제한으로 알고 있습니다!
-// 누락된 사항은 email.properties 참고 바랍니다.
 public class EmailSendConfig {
 
-    @Value("${mail.smtp.port}")
+    @Value("${spring.mail.port}")
     private int port;
 
-    @Value("${mail.smtp.socketFactory.port}")
-    private int socketPort;
+    @Value("${spring.mail.username}")
+    private String username;
 
-    @Value("${mail.smtp.auth}")
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
     private boolean auth;
 
-    @Value("${mail.smtp.starttls.enable}")
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
     private boolean starttls;
 
-    @Value("${mail.smtp.starttls.required}")
-    private boolean isStarttls;
+    @Value("${spring.mail.properties.mail.smtp.starttls.required}")
+    private boolean starttlsRequired;
 
-    @Value("${mail.smtp.socketFactory.fallback}")
-    private boolean fallback;
+    @Value("${spring.mail.properties.mail.smtp.connection-timeout}")
+    private int connectionTimeout;
 
-    @Value("${AdminMail.id}")
-    private String id;
+    @Value("${spring.mail.properties.mail.smtp.timeout}")
+    private int timeout;
 
-    @Value("${AdminMail.password}")
-    private String password;
+    @Value("${spring.mail.properties.mail.smtp.write-timeout}")
+    private int writeTimeout;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         javaMailSender.setHost("smtp.gmail.com");
-        javaMailSender.setUsername(id);
+        javaMailSender.setUsername(username);
         javaMailSender.setPassword(password);
         javaMailSender.setPort(port);
         javaMailSender.setJavaMailProperties(getMailProperties());
@@ -54,11 +52,12 @@ public class EmailSendConfig {
 
     private Properties getMailProperties() {
         Properties properties = new Properties();
-        properties.put("mail.smtp.socketFactory.port", socketPort);
         properties.put("mail.smtp.auth", auth);
         properties.put("mail.smtp.starttls.enable", starttls);
-        properties.put("mail.smtp.starttls.required", isStarttls);
-        properties.put("mail.smtp.soketFactory.fallback", fallback);
+        properties.put("mail.smtp.starttls.required", starttlsRequired);
+        properties.put("mail.smtp.connection-timeout", connectionTimeout);
+        properties.put("mail.smtp.timeout", timeout);
+        properties.put("mail.smtp.write-timeout", writeTimeout);
         properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         return properties;
     }
