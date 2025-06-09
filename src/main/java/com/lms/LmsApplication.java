856dcc87fd344f89c1ca1089361273f1d0c84a9b
RosieOh <dhxogns920@gmail.com>
2025-06-09 13:37:32 +0900

    CHORE : AWS SDK 설정 제거

--- src/main/java/com/lms/LmsApplication.java
+++ src/main/java/com/lms/LmsApplication.java
@@ -10,10 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class LmsApplication {
 
-    static {
-        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(LmsApplication.class, args);
     }
